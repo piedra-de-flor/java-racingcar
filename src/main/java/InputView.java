ae201a1bca69a06bc16d7dc309ab95805cecdb98
piedra-de-flor <xxspdlqjxx@naver.com>
2023-06-20 16:33:37 +0900

    Refactor(InputView) : add function to drive exception for InputMismatchException

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,14 +1,25 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     static Scanner scanner = new Scanner(System.in);
     public int getCarNumber() {
         System.out.println("자동차 대수는 몇 대인가요?");
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("input error : 양의 정수값을 입력해야 합니다.");
+            return getCarNumber();
+        }
     }
 
     public int getTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("input error : 양의 정수값을 입력해야 합니다.");
+            return getTryCount();
+        }
     }
 }
