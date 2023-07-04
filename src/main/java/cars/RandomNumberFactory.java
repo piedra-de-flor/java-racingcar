ac9d708bed5983ece7cf6f089611a57b13660045
piedra-de-flor <xxspdlqjxx@naver.com>
2023-07-04 22:59:04 +0900

    Style(RandomNumberFactory) : rename final number

--- src/main/java/cars/RandomNumberFactory.java
+++ src/main/java/cars/RandomNumberFactory.java
@@ -1,8 +1,8 @@
 package cars;
 
 public class RandomNumberFactory {
-    private final int MULTIPLE_NUMBER_FOR_RANDOM_NUMBER_BETWEEN_0_TO_10 = 10;
+    private final int SCALE_FACTOR_ZERO_TO_TEN = 10;
     public double makeRandomNumber() {
-        return Math.random() * MULTIPLE_NUMBER_FOR_RANDOM_NUMBER_BETWEEN_0_TO_10;
+        return Math.random() * SCALE_FACTOR_ZERO_TO_TEN;
     }
 }
