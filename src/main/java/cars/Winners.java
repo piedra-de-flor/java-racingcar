903170f9066c4c3f8758f9b9c8d6f4f66f2fe1a7
piedra-de-flor <xxspdlqjxx@naver.com>
2023-07-10 18:05:26 +0900

    Refactor(Winners) : remove if else commands

--- src/main/java/cars/Winners.java
+++ src/main/java/cars/Winners.java
@@ -2,11 +2,12 @@ package cars;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Winners {
     private final List<Car> winners;
-    private int maxLocation = Integer.MIN_VALUE;
+    List<Car> sortedCars;
 
     public Winners(List<Car> cars) {
         this.winners = new ArrayList<>();
@@ -14,20 +15,23 @@ public class Winners {
     }
 
     private void findWinners(List<Car> cars) {
-        for (Car car : cars) {
-            compareLocation(car);
-        }
+        sortedCars = new ArrayList<>(cars);
+        sortedCars.sort(new CarLocationComparator());
+
+        makeWinners(sortedCars);
     }
 
-    private void compareLocation(Car car) {
-        if (car.getLocation() == maxLocation) {
-            winners.add(car);
-        }
+    private void makeWinners(List<Car> sortedCars) {
+        final int maxLocation = sortedCars.get(0).getLocation();
+        sortedCars.stream()
+                .filter(car -> car.getLocation() == maxLocation)
+                .forEach(winners::add);
+    }
 
-        if (car.getLocation() > maxLocation) {
-            maxLocation = car.getLocation();
-            winners.clear();
-            winners.add(car);
+    private static class CarLocationComparator implements Comparator<Car> {
+        @Override
+        public int compare(Car car1, Car car2) {
+            return Integer.compare(car2.getLocation(), car1.getLocation());
         }
     }
 
